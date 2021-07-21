e632485d5b6992b27056efbc6c212418e23b069e
Dioa <xiaomiaomail@163.com>
2021-07-21 17:10:39 +0800

    feat: add login type

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,10 +1,26 @@
 <template>
   <div class="login">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      :validate-on-rule-change="false"
+      v-show="loginType == 0"
+    >
       <h3 class="title">威克后台管理系统</h3>
       <el-form-item prop="username">
-        <el-input v-model="loginForm.username" type="text" auto-complete="off" placeholder="账号">
-          <svg-icon slot="prefix" icon-class="user" class="el-input__icon input-icon" />
+        <el-input
+          v-model="loginForm.username"
+          type="text"
+          auto-complete="off"
+          placeholder="账号"
+        >
+          <svg-icon
+            slot="prefix"
+            icon-class="user"
+            class="el-input__icon input-icon"
+          />
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
@@ -15,7 +31,11 @@
           placeholder="密码"
           @keyup.enter.native="handleLogin"
         >
-          <svg-icon slot="prefix" icon-class="password" class="el-input__icon input-icon" />
+          <svg-icon
+            slot="prefix"
+            icon-class="password"
+            class="el-input__icon input-icon"
+          />
         </el-input>
       </el-form-item>
       <el-form-item prop="code">
@@ -26,13 +46,21 @@
           style="width: 63%"
           @keyup.enter.native="handleLogin"
         >
-          <svg-icon slot="prefix" icon-class="validCode" class="el-input__icon input-icon" />
+          <svg-icon
+            slot="prefix"
+            icon-class="validCode"
+            class="el-input__icon input-icon"
+          />
         </el-input>
         <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+          <img :src="codeUrl" @click="getCode" class="login-code-img" />
         </div>
       </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+      <div class="operator-box">
+        <el-checkbox v-model="loginForm.rememberMe">记住密码</el-checkbox>
+
+        <el-button type="text" @click="loginTypeChange">验证码登录</el-button>
+      </div>
       <el-form-item style="width:100%;">
         <el-button
           :loading="loading"
@@ -46,17 +74,70 @@
         </el-button>
       </el-form-item>
     </el-form>
+
+    <el-form
+      ref="loginByPhoneForm"
+      :model="loginByPhoneForm"
+      :rules="loginByPhoneRules"
+      class="login-form"
+      :validate-on-rule-change="false"
+      v-show="loginType == 1"
+    >
+      <h3 class="title">威克后台管理系统</h3>
+      <el-form-item prop="phonenumber">
+        <el-input
+          v-model="loginByPhoneForm.phonenumber"
+          type="text"
+          auto-complete="off"
+          placeholder="手机号码"
+          prefix-icon="el-icon-mobile"
+          @keyup.enter.native="loginByPhoneHandler"
+        >
+          <template slot="append">
+            <el-button @click="sendSmsHandler">{{ sendText }}</el-button>
+          </template>
+        </el-input>
+      </el-form-item>
+      <el-form-item prop="code">
+        <el-input
+          v-model="loginByPhoneForm.code"
+          auto-complete="off"
+          placeholder="验证码"
+          prefix-icon="el-icon-chat-round"
+          @keyup.enter.native="loginByPhoneHandler"
+        >
+        </el-input>
+      </el-form-item>
+      <div class="operator-box">
+        <el-button type="text" @click="loginTypeChange">账号登录</el-button>
+      </div>
+      <el-form-item style="width:100%;">
+        <el-button
+          :loading="loading"
+          size="medium"
+          type="primary"
+          style="width:100%;"
+          @click.native.prevent="loginByPhoneHandler"
+        >
+          <span v-if="!loading">登 录</span>
+          <span v-else>登 录 中...</span>
+        </el-button>
+      </el-form-item>
+    </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-{{ new Date().getFullYear() }} csvic.com All Rights Reserved.</span>
+      <span
+        >Copyright © 2018-{{ new Date().getFullYear() }} csvic.com All Rights
+        Reserved.</span
+      >
     </div>
   </div>
 </template>
 
 <script>
-import { getCodeImg } from "@/api/login";
+import { getCodeImg, sendSms, loginByPhone } from "@/api/login";
 import Cookies from "js-cookie";
-import { encrypt, decrypt } from '@/utils/jsencrypt'
+import { encrypt, decrypt } from "@/utils/jsencrypt";
 
 export default {
   name: "Login",
@@ -71,6 +152,11 @@ export default {
         code: "",
         uuid: ""
       },
+      loginByPhoneForm: {
+        phonenumber: "",
+        code: ""
+      },
+      loginType: 0, // 0: 账号密码登录，1: 验证码登录
       loginRules: {
         username: [
           { required: true, trigger: "blur", message: "用户名不能为空" }
@@ -80,8 +166,16 @@ export default {
         ],
         code: [{ required: true, trigger: "change", message: "验证码不能为空" }]
       },
+      loginByPhoneRules: {
+        phonenumber: [
+          { required: true, trigger: "blur", message: "手机号码不能为空" }
+        ],
+        code: [{ required: true, trigger: "blur", message: "验证码不能为空" }]
+      },
       loading: false,
-      redirect: undefined
+      redirect: undefined,
+      sendText: "发送验证码",
+      timeId: null
     };
   },
   watch: {
@@ -106,12 +200,61 @@ export default {
     getCookie() {
       const username = Cookies.get("username");
       const password = Cookies.get("password");
-      const rememberMe = Cookies.get('rememberMe')
+      const rememberMe = Cookies.get("rememberMe");
       this.loginForm = {
         username: username === undefined ? this.loginForm.username : username,
-        password: password === undefined ? this.loginForm.password : decrypt(password),
+        password:
+          password === undefined ? this.loginForm.password : decrypt(password),
         rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
       };
+    },
+    // 发送验证码
+    sendSmsHandler() {
+      if (!this.loginByPhoneForm.phonenumber) {
+        this.$message.error("请输入手机号码");
+        return;
+      }
+      sendSms(this.loginByPhoneForm.phonenumber).then(res => {
+        if (res.code == 200) {
+          this.$refs.loginByPhoneForm.clearValidate();
+          this.$notify({
+            type: "success",
+            title: "成功提示",
+            message: "发送验证码成功!"
+          });
+          this.sendText = "60s";
+          this.timeId = setInterval(() => {
+            this.sendText = this.sendText.split("s")[0] - 1 + "s";
+            if (this.sendText.split("s")[0] <= 0) {
+              clearInterval(this.timeId);
+              this.sendText = "发送验证码";
+            }
+          }, 1000);
+        }
+      });
+    },
+    loginTypeChange() {
+      this.loginType = this.loginType == 0 ? 1 : 0;
+      setTimeout(() => {
+        this.$refs.loginByPhoneForm.clearValidate();
+        this.$refs.loginForm.clearValidate();
+      }, 0);
+    },
+    async loginByPhoneHandler() {
+      this.$refs.loginByPhoneForm.validate(valid => {
+        if (valid) {
+          this.loading = true;
+          this.$store
+            .dispatch("loginByPhone", this.loginByPhoneForm)
+            .then(() => {
+              this.$router.push({ path: this.redirect || "/" }).catch(() => {});
+            })
+            .catch(() => {
+              this.loading = false;
+              this.getCode();
+            });
+        }
+      });
     },
     handleLogin() {
       this.$refs.loginForm.validate(valid => {
@@ -119,19 +262,26 @@ export default {
           this.loading = true;
           if (this.loginForm.rememberMe) {
             Cookies.set("username", this.loginForm.username, { expires: 30 });
-            Cookies.set("password", encrypt(this.loginForm.password), { expires: 30 });
-            Cookies.set('rememberMe', this.loginForm.rememberMe, { expires: 30 });
+            Cookies.set("password", encrypt(this.loginForm.password), {
+              expires: 30
+            });
+            Cookies.set("rememberMe", this.loginForm.rememberMe, {
+              expires: 30
+            });
           } else {
             Cookies.remove("username");
             Cookies.remove("password");
-            Cookies.remove('rememberMe');
+            Cookies.remove("rememberMe");
           }
-          this.$store.dispatch("Login", this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || "/" }).catch(()=>{});
-          }).catch(() => {
-            this.loading = false;
-            this.getCode();
-          });
+          this.$store
+            .dispatch("Login", this.loginForm)
+            .then(() => {
+              this.$router.push({ path: this.redirect || "/" }).catch(() => {});
+            })
+            .catch(() => {
+              this.loading = false;
+              this.getCode();
+            });
         }
       });
     }
@@ -199,5 +349,12 @@ export default {
 }
 .login-code-img {
   height: 38px;
+}
+
+.operator-box {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
 }
 </style>
